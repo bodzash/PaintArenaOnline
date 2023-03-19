244668e32062118a0a336231ced0a63211f913b8
bodzash <bozzaylaszlo4@gmail.com>
2023-03-19 09:50:40 +0100

    Added server prefabs

--- Game/Server/Source/Server.cpp
+++ Game/Server/Source/Server.cpp
@@ -11,6 +11,7 @@
 #include "Math.hpp"
 #include "Components.hpp"
 #include "NetworkTypes.hpp"
+#include "SvPrefabs.hpp"
 #include "Systems/BulletSystems.hpp"
 #include "Systems/MovementSystems.hpp"
 #include "Systems/BoundsSystems.hpp"
@@ -20,36 +21,6 @@
 
 using std::string;
 using namespace std::chrono;
-
-entt::entity CreatePrefabPlayer(entt::registry& Scene, uint8_t NetId)
-{
-  entt::entity Player = Scene.create();
-  Scene.emplace<PlayerTag>(Player);
-  Scene.emplace<TeamId>(Player, NetId);
-  Scene.emplace<NetworkId>(Player, NetId);
-  Scene.emplace<Position>(Player, (float)RandomRange(4, 236), (float)RandomRange(4, 172));
-  Scene.emplace<Velocity>(Player);
-  Scene.emplace<Speed>(Player, 140.0f, 45.0f, 27.0f);
-  Scene.emplace<Collider>(Player, 8.0f);
-  Scene.emplace<Health>(Player, 0, 100);
-  Scene.emplace<PlayerInput>(Player);
-
-  return Player;
-}
-
-entt::entity CreatePrefabBullet(entt::registry& Scene, float x, float y, float Angle,
-  uint8_t NetId)
-{
-  entt::entity Bullet = Scene.create();
-  Scene.emplace<BulletTag>(Bullet);
-  Scene.emplace<TeamId>(Bullet, NetId);
-  Scene.emplace<Position>(Bullet, x, y);
-  Scene.emplace<Direction>(Bullet, Angle);
-  Scene.emplace<Collider>(Bullet, 4.0f);
-  Scene.emplace<Speed>(Bullet, 140.0f);
-
-  return Bullet;
-}
 
 // Program entry point
 int main()
