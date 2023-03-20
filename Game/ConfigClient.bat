baa8dec3691d29e12d09c7d6e32bd8f6662a3208
bodzash <bozzaylaszlo4@gmail.com>
2023-03-20 12:14:48 +0100

    Corrected client config

--- Game/ConfigClient.bat
+++ Game/ConfigClient.bat
@@ -1,1 +1,1 @@
-cmake -G "MinGW Makefiles" -S . -B Build/Client -DCMAKE_BUILD_TYPE=Debug -DOPENGL_VERSION=2.1 -DIS_CLIENT=ON
+cmake -G "MinGW Makefiles" -S . -B Build/Client -DCMAKE_BUILD_TYPE=Debug -DGRAPHICS=GRAPHICS_API_OPENGL_21 -DIS_CLIENT=ON
